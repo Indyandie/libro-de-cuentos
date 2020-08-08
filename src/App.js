0edf65ea83b380ae2c58d706c481fb9caf1777ab
Andy Contreras <andy.j.contreras@me.com>
2020-08-08 16:19:13 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.less'
+
+const App = () => <main id="app">
+   <h1 className="emoji">
+      <div>⚛️</div>
+      📦
+   </h1>
+   <h2>Basic <strong>AF</strong> React Parcel</h2>
+</main>
+export default App
