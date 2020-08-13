4125c80a01bb1f09d01bd744991dc59d8babb7ed
Andy Contreras <andy.j.contreras@me.com>
2020-08-12 23:25:20 -0500

    knobs, essenttials, source

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,22 +1,25 @@
-const path = require('path')
+// const path = require('path')
 
 module.exports = {
-  stories: ['../src/stories/**/*.stories.js'],
+  stories: ['../stories/**/*.stories.js'],
   addons: [
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
+    // '@storybook/preset-create-react-app',
+    '@storybook/addon-essentials',
+    '@storybook/addon-storysource',
+    // '@storybook/addon-actions',
+    // '@storybook/addon-knobs',
+    // '@storybook/addon-links',
   ],
 
   // webpackFinal: (config) => console.dir(config, { depth: null }) || config,
-
+  
   webpackFinal: async (config) => {
 
     // Make whatever fine-grained changes you need
 
     config.module.rules.push({
-        test: /\.css$/,
-        loader: "css-loader"
+      test: /\.css$/,
+      loader: "css-loader"
     })
 
     config.module.rules.push({
---
+++ .storybook/preview.js
@@ -0,0 +1,19 @@
+export const parameters = {
+   backgrounds: {
+      default: 'dark',
+      values: [
+         { 
+            name: 'dark', 
+            value: '#333333'
+         },
+         { 
+            name: 'gray', 
+            value: '#cdcdcd' 
+         },
+         { 
+            name: 'color', 
+            value: '#2a91f8' 
+         },
+      ],
+   }
+}
